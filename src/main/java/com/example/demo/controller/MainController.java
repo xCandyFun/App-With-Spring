f1a63b584fecd014c492420f8f166016f298a22b
PC Simon <simtho0624@gmail.com>
2024-12-17 12:51:06 +0100

    Add a controller to take care of the html file

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/index")
+    public String StartPage(){
+        return "StartPage";
+    }
+}
